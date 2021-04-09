5ec7f41e779b585cd682cd4b869699dc8728eb76
Kevin Zheng <kevinzhenng@gmail.com>
2021-04-09 04:38:47 -0400

    Move to hetzner

--- kubernetes/scripts/rook-teardown.sh
+++ kubernetes/scripts/rook-teardown.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-devices=(a3 b3 c d)
+devices=(nvme0n1p3 nvme1n1p3)
 
 for device in "${devices[@]}"; do
-    DISK="/dev/sd${device}"
+    DISK="/dev/${device}"
     sgdisk --zap-all $DISK
-    dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
     blkdiscard $DISK
 done
 
