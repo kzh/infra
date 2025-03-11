5c4939ded6a5bf6e6d9163dfe9c300b00a7c558d
kevin <me@kzh.dev>
2025-03-11 15:01:39 -0400

    pulumi: add tailscale k8s api server proxy

--- pulumi/tailscale/index.ts
+++ pulumi/tailscale/index.ts
@@ -2,23 +2,30 @@ import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
 const namespace = new k8s.core.v1.Namespace("tailscale", {
-    metadata: {
-        name: "tailscale",
-    }
+	metadata: {
+		name: "tailscale",
+	},
 });
 
 const config = new pulumi.Config();
-const operator = new k8s.helm.v3.Chart("tailscale-operator", {
-    chart: "tailscale-operator",
-    namespace: "tailscale",
-    version: "1.76.6",
-    fetchOpts: {
-        repo: "https://pkgs.tailscale.com/helmcharts",
-    },
-    values: {
-        oauth: {
-            clientId: config.require("TS_CLIENT_ID"),
-            clientSecret: config.requireSecret("TS_CLIENT_SECRET"),
-        }
-    }
-}, { dependsOn: [namespace] });
+const operator = new k8s.helm.v3.Chart(
+	"tailscale-operator",
+	{
+		chart: "tailscale-operator",
+		namespace: "tailscale",
+		version: "1.80.3",
+		fetchOpts: {
+			repo: "https://pkgs.tailscale.com/helmcharts",
+		},
+		values: {
+			oauth: {
+				clientId: config.require("TS_CLIENT_ID"),
+				clientSecret: config.requireSecret("TS_CLIENT_SECRET"),
+			},
+			apiServerProxyConfig: {
+				mode: "true",
+			},
+		},
+	},
+	{ dependsOn: [namespace] },
+);
