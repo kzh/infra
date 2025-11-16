ab8b9735806e6a24f2863d2dd311582b96a66ca6
Kevin <me@kzh.dev>
2025-11-16 06:37:39 -0500

    feat: update jupyterhub chart and image build

--- pulumi/data/analytics/jupyterhub/__main__.py
+++ pulumi/data/analytics/jupyterhub/__main__.py
@@ -1,59 +1,69 @@
 import pulumi
 import pulumi_kubernetes as k8s
 
-config = pulumi.Config()
-jupyterhub_namespace = k8s.core.v1.Namespace(
+NAMESPACE = "jhub"
+HOSTNAME = "jupyterhub"
+
+namespace = k8s.core.v1.Namespace(
     "jupyterhub-namespace",
-    metadata=k8s.meta.v1.ObjectMetaArgs(
-        name=config.require("namespace"),
-    ),
+    metadata=k8s.meta.v1.ObjectMetaArgs(name=NAMESPACE),
 )
 
 jupyterhub_chart = k8s.helm.v4.Chart(
     "jupyterhub",
     chart="jupyterhub",
+    namespace=NAMESPACE,
+    version="4.3.1",
     repository_opts=k8s.helm.v4.RepositoryOptsArgs(
         repo="https://hub.jupyter.org/helm-chart/",
     ),
-    namespace=jupyterhub_namespace.metadata.name,
     values={
-        "proxy": {
-            "service": {"type": "ClusterIP"},
-            "chp": {
-                "networkPolicy": {
-                    "enabled": False,
-                }
-            },
-            "traefik": {
-                "networkPolicy": {
-                    "enabled": False,
-                }
-            },
-        },
-        "singleuser": {
-            "storage": {"capacity": "100Gi"},
-            "networkPolicy": {
-                "enabled": False,
-            },
-        },
         "ingress": {
             "enabled": True,
             "ingressClassName": "tailscale",
-            "tls": [
-                {
-                    "hosts": ["jupyter"],
-                }
-            ],
+            "hosts": [HOSTNAME],
+            "tls": [{"hosts": [HOSTNAME]}],
+        },
+        "proxy": {
+            "service": {
+                "type": "ClusterIP",
+            }
         },
         "hub": {
-            "networkPolicy": {
-                "enabled": False,
+            "db": {
+                "type": "sqlite-pvc",
+                "pvc": {
+                    "storage": "5Gi",
+                    "accessModes": ["ReadWriteOnce"],
+                },
+            },
+            "networkPolicy": {"enabled": False},
+        },
+        "singleuser": {
+            "startTimeout": 600,
+            "image": {
+                "name": "ghcr.io/kzh/jupyter",
+                "tag": "py312-amd64",
+                "pullPolicy": "Always",
+            },
+            "storage": {
+                "type": "dynamic",
+                "capacity": "60Gi",
+                "dynamic": {
+                    "storageAccessModes": ["ReadWriteOnce"],
+                },
             }
+        },
+        "cull": {
+            "enabled": False,
         },
         "scheduling": {
-            "userScheduler": {
-                "replicas": 1,
-            }
+            "userScheduler": {"enabled": False},
+            "userPlaceholder": {"enabled": False},
+            "podPriority": {"enabled": False},
+            "corePods": {"nodeAffinity": {"matchNodePurpose": "ignore"}},
+            "userPods": {"nodeAffinity": {"matchNodePurpose": "ignore"}},
         },
     },
+    opts=pulumi.ResourceOptions(depends_on=[namespace]),
 )
